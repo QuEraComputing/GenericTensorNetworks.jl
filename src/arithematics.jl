f7e3bff39ba044d809bda19a7e629a0202b1a367
GiggleLiu <cacate0129@gmail.com>
2022-02-10 14:57:40 -0500

    skip CI

--- src/arithematics.jl
+++ src/arithematics.jl
@@ -180,6 +180,12 @@ julia> b = ConfigEnumerator([StaticBitVector([0,0,0,0,0]), StaticBitVector([1,0,
 
 julia> a + b
 {11100, 10001, 00000, 10101}
+
+julia> one(a)
+{00000}
+
+julia> zero(a)
+{}
 ```
 """
 struct ConfigEnumerator{N,S,C}
@@ -223,6 +229,9 @@ The algebra for sampling one configuration, where `N` is the length of configura
 `C` is the size of storage in unit of `UInt64`,
 `S` is the bit width to store a single element in a configuration, i.e. `log2(# of flavors)`, for bitstrings, it is `1``.
 
+!!! note
+    `ConfigSampler` is a **probabilistic** commutative semiring, adding two config samplers do not give you deterministic results.
+
 Example
 ----------------------
 ```jldoctest; setup=:(using GraphTensorNetworks, Random; Random.seed!(2))
@@ -234,6 +243,12 @@ ConfigSampler{5, 1, 1}(10100)
 
 julia> ConfigSampler(StaticBitVector([1,1,1,0,0])) * ConfigSampler(StaticBitVector([0,0,0,0,1]))
 ConfigSampler{5, 1, 1}(11101)
+
+julia> one(ConfigSampler{5, 1, 1})
+ConfigSampler{5, 1, 1}(00000)
+
+julia> zero(ConfigSampler{5, 1, 1})
+ConfigSampler{5, 1, 1}(11111)
 ```
 """
 struct ConfigSampler{N,S,C}

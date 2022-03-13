b4d2a4af41f49415b2ad1200c4269d93c0828238
GiggleLiu <cacate0129@gmail.com>
2022-03-12 21:18:14 -0500

    update

--- examples/Others.jl
+++ examples/Others.jl
@@ -1,10 +1,12 @@
 # # Other problems can be solved by GraphTensorNetworks
 
 # ### Set packing problem
-# It is a generalization of the independent set problem.
+# It is a generalization of the independent set problem from simple graph to hypergraph.
 
 # ### Vertex cover problem
-# A vertex cover is a complement of an independent set.
+# In graph theory, a vertex cover of a graph is a set of vertices that includes at least one endpoint of every edge of the graph.
+# Let ``G=(V,E)`` be a graph, a set of vertices ``X \subset V`` is independent if and only if its complement ``V \backslash X`` is a vertex cover.
+# Hence the problem of finding vertex covers of size ``k`` is equivalent to finding independent sets of size ``|V|-k``.
 
 # ### Maximal clique problem
 # The maximal clique of graph ``G`` is a maximal clique of ``G``'s complement graph.

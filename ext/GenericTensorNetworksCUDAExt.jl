140c99aada16c4236d1c05c8a82a90d7349ed091
GiggleLiu <cacate0129@gmail.com>
2023-10-14 16:21:30 +0800

    fix cuda one-hot mask

--- ext/GenericTensorNetworksCUDAExt.jl
+++ ext/GenericTensorNetworksCUDAExt.jl
@@ -10,13 +10,20 @@ import GenericTensorNetworks: onehotmask!, togpu
 togpu(x::AbstractArray) = CuArray(x)
 
 function onehotmask!(A::CuArray{T}, X::CuArray{T}) where T
+    return reshape(onehotmaskvec!(vec(A), vec(X)), size(A))
+end
+function onehotmaskvec!(A::CuVector{T}, X::CuVector{T}) where T
     @assert length(A) == length(X)
     mask = X .≈ inv.(A)
     ci = argmax(mask)
     mask .= false
-    CUDA.@allowscalar mask[ci] = true
-    # set some elements in X to zero to help back propagating.
-    X[(!).(mask)] .= Ref(zero(T))
+    CUDA.@allowscalar begin
+        mask[ci] = true
+        # set only one element in X to nonzero to help back propagating.
+        xi = X[ci]
+        fill!(X, zero(T))
+        X[ci] = xi
+    end
     return mask
 end
 

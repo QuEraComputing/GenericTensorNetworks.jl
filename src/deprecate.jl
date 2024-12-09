38cbf7f4d1bbbfabfe4eea5f8c6cdcf017ca9a03
c-allergic <xli445614@gmail.com>
2024-12-09 14:28:28 +0800

    deprecate GraphProblem, chweights,ZeroWeight and UnitWeight

--- src/deprecate.jl
+++ src/deprecate.jl
@@ -1,8 +1,12 @@
 @deprecate Independence(args...; kwargs...) IndependentSet(args...; kwargs...)
 @deprecate MaximalIndependence(args...; kwargs...) MaximalIS(args...; kwargs...)
-@deprecate NoWeight() UnitWeight()
+@deprecate UnitWeight() error("UnitWeight() is deprecated. Please Use UnitWeight(::Int) instead.")
+@deprecate ZeroWeight() error("ZeroWeight() is deprecated. Please Use ZeroWeight(::Int) instead.")
 @deprecate HyperSpinGlass(args...; kwargs...) SpinGlass(args...; kwargs...)
 
 @deprecate nflavor(args...; kwargs...) num_flavors(args...; kwargs...)
 @deprecate labels(args...; kwargs...) variables(args...; kwargs...)
 @deprecate get_weights(args...; kwargs...) weights(args...; kwargs...)
+@deprecate chweights(args...; kwargs...) set_weights(args...; kwargs...)
+
+@deprecate GraphProblem() ConstraintSatisfactionProblem()
